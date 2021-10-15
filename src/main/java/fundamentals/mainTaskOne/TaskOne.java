42fa2eb5a1f584436dfd97a592bf0aabf3de057a
Yuliya Ralavets <julia.baranchik@gmail.com>
2021-10-15 12:28:07 +0300

    doing task one

--- src/main/java/fundamentals/mainTaskOne/TaskOne.java
+++ src/main/java/fundamentals/mainTaskOne/TaskOne.java
@@ -1,4 +1,9 @@
 package fundamentals.mainTaskOne;
 
 public class TaskOne {
+    public static void main(String[] args) {
+        if(args.length > 0) {
+            System.out.println("Hello, " + args[0] + "!");
+        }
+    }
 }
