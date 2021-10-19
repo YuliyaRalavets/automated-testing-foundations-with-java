2f7c1a5500b1d134439f3271d2d2bfcef10d312a
Yuliya Ralavets <julia.baranchik@gmail.com>
2021-10-19 10:09:51 +0300

    doing main task for collections(add TuberVegetable)

---
+++ src/main/java/collections/optional/main/vegetable/tuberVegetable/Potato.java
@@ -0,0 +1,13 @@
+package collections.optional.main.vegetable.tuberVegetable;
+
+public class Potato extends TuberVegetable{
+
+    public Potato(double weight){
+        super(77, weight);
+    }
+
+    @Override
+    public String getName() {
+        return "Potato";
+    }
+}
---
+++ src/main/java/collections/optional/main/vegetable/tuberVegetable/SweetPotato.java
@@ -0,0 +1,13 @@
+package collections.optional.main.vegetable.tuberVegetable;
+
+public class SweetPotato extends TuberVegetable {
+
+    public SweetPotato(double weight){
+        super(86, weight);
+    }
+
+    @Override
+    public String getName() {
+        return "Sweet potato";
+    }
+}
