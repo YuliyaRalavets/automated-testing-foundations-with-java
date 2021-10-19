f1bf7a01b9fda0462e53ab58d13483e61e126e55
Yuliya Ralavets <julia.baranchik@gmail.com>
2021-10-19 09:59:00 +0300

    doing main task for collections(add NightshadeVegetable)

---
+++ src/main/java/collections/optional/main/vegetable/nightshadeVegetable/ChiliPepper.java
@@ -0,0 +1,12 @@
+package collections.optional.main.vegetable.nightshadeVegetable;
+
+public class ChiliPepper extends NightshadeVegetable {
+    public ChiliPepper(double weight){
+        super(40, weight);
+    }
+
+    @Override
+    public String getName() {
+        return "Chili pepper";
+    }
+}
