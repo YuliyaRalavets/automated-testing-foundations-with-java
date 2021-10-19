f5dbf9751f1c2c866c89555b1e8102cf7955c292
Yuliya Ralavets <julia.baranchik@gmail.com>
2021-10-19 10:05:34 +0300

    doing main task for collections(add SaladVegetable)

---
+++ src/main/java/collections/optional/main/vegetable/saladVegetable/HeadSalad.java
@@ -0,0 +1,13 @@
+package collections.optional.main.vegetable.saladVegetable;
+
+public class HeadSalad extends SaladVegetable{
+
+    public HeadSalad(double weight){
+        super(14, weight);
+    }
+
+    @Override
+    public String getName() {
+        return "Head salad";
+    }
+}
