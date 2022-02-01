87834a628fada8c1552e19fe148b02192f2e15e6
Yuliya Ralavets <julia.baranchik@gmail.com>
2022-02-01 12:28:21 +0300

    refactoring main task for IO

--- src/main/java/IO/MainTask.java
+++ src/main/java/IO/MainTask.java
@@ -1,40 +1,58 @@
 package IO;
 
-
 import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.*;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
 import java.util.stream.Collectors;
 
 public class MainTask {
     public static void main(String[] args) {
-
-       MainTask mainTask = new MainTask();
-        try {
-            printTree(Paths.get("D:/YULIYA/aircompany"), "");
-            //printTree(Paths.get("D:/!!! ВИДЕООПЕРАТОР !!!/DOC/калыханка"), "");
-        } catch (IOException e) {
-            e.printStackTrace();
+        if (args.length == 0 || args.length > 1 || args[0].length() == 0)
+            System.out.println("Incorrect input args.");
+        else {
+            String inputFileName = args[0];
+            Path input = Paths.get(inputFileName);
+            if (input.toFile().exists()) {
+                if (input.toFile().isDirectory()) {
+                    Charset charset = Charset.forName("UTF-8");
+                    try (PrintWriter writer = new PrintWriter(Files.newBufferedWriter(Paths.get("fileformain.txt"), charset))) {
+                        printTree(input, "", writer);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                } else {
+                    if (input.toFile().isFile() && input.endsWith(".txt")) {
+                        infoAboutFile(input);
+                    } else System.out.println("Incorrect input args. You need to specify the path to the txt file.");
+                }
+            } else System.out.println("Input path is not exist.");
         }
     }
 
-    public static void printTree(Path path, String mask) throws IOException {
-        System.out.println(path.getFileName());
+    private static void printTree(Path path, String space, PrintWriter writer) throws IOException {
+        writer.println(path.getFileName());
         if (path.toFile().isDirectory()) {
             List<Path> paths = Files.list(path)
                     .sorted(Comparator.comparing((Path p) -> p.toFile().isDirectory()).thenComparing(Path::getFileName))
                     .collect(Collectors.toList());
-            List<Path> dirs = paths.stream().filter(p-> p.toFile().isDirectory()).collect(Collectors.toList());
+            List<Path> dirs = paths
+                    .stream()
+                    .filter(p -> p.toFile().isDirectory())
+                    .collect(Collectors.toList());
             for (int i = 0; i < paths.size(); i++) {
                 if (paths.get(i).toFile().isFile()) {
-                    System.out.printf("%s%s ", mask, dirs.size() == 0 ? " " : "\u2502 ");
-                    printTree(paths.get(i), mask);
+                    writer.printf("%s%s ", space, dirs.size() == 0 ? " " : "\u2502  ");
+                    printTree(paths.get(i), space, writer);
                 } else {
-                    System.out.printf("%s%s\u2500\u2500\u2500", mask, i == paths.size() - 1 ? "\u2514" : "\u251c");
-                    printTree(paths.get(i), String.format("%s%s", mask, i == paths.size() - 1 ? "   " : "\u2502   "));
+                    writer.printf("%s%s\u2500\u2500\u2500", space, i == paths.size() - 1 ? "\u2514" : "\u251c");
+                    printTree(paths.get(i), String.format("%s%s", space, i == paths.size() - 1 ? "   " : "\u2502  "), writer);
                 }
             }
         }
@@ -53,45 +71,49 @@ public class MainTask {
         long filesCount = files;
         long dirsCount = dirs;
         Path root = path.getRoot();
-        //System.out.println();
-        //System.out.println(path);
         if (root.equals(path)) {
             filesCount += countOfFilesInPath(path);
-            dirsCount += contOfDirInPath(path);
-            //System.out.println(countOfFilesInPath(path) + " files");
-            //System.out.println(contOfDirInPath(path)+ " dirs");
-            //System.out.println("len " + lengthOfFileName(path));
+            dirsCount += countOfDirInPath(path);
             len += lengthOfFileName(path);
-            System.out.println("Total Dir "  + dirsCount);
+            System.out.println("Total Dirs "  + dirsCount);
             System.out.println("Total Files " + filesCount);
-            //System.out.println("Total file name length is "+ len);
             System.out.printf("Average length is %.2f", (double)len/filesCount);
             System.out.printf("\nAverage number of files per folder is %.2f", (double)filesCount/count );
-
         }else {
             filesCount += countOfFilesInPath(path);
-            dirsCount += contOfDirInPath(path);
-            //System.out.println(countOfFilesInPath(path) + " files");
-            //System.out.println(contOfDirInPath(path)+ " dirs");
-            //System.out.println(lengthOfFileName(path));
+            dirsCount += countOfDirInPath(path);
             len += lengthOfFileName(path);
             textInfo(path.getParent(),filesCount,dirsCount,len,count);
         }
     }
 
-    private static long contOfDirInPath(Path path) {
-        List<File> files = Arrays.stream((path.toFile().listFiles())).collect(Collectors.toList());
-        return files.stream().filter(File::isDirectory).count();
+    private static long countOfDirInPath(Path path) {
+        List<File> files = Arrays
+                .stream((path.toFile().listFiles()))
+                .collect(Collectors.toList());
+        return files
+                .stream()
+                .filter(File::isDirectory)
+                .count();
     }
 
     private static long countOfFilesInPath(Path path) {
-        List<File> files = Arrays.stream((path.toFile().listFiles())).collect(Collectors.toList());
-        return files.stream().filter(File::isFile).count();
+        List<File> files = Arrays
+                .stream((path.toFile().listFiles()))
+                .collect(Collectors.toList());
+        return files
+                .stream()
+                .filter(File::isFile)
+                .count();
     }
 
     private static double lengthOfFileName(Path path){
         int length = 0;
-        List<File> files = Arrays.stream((path.toFile().listFiles())).collect(Collectors.toList()).stream().filter(File::isFile).collect(Collectors.toList());
+        List<File> files = Arrays
+                .stream((path.toFile().listFiles()))
+                .collect(Collectors.toList())
+                .stream().filter(File::isFile)
+                .collect(Collectors.toList());
         for(File f: files){
             length += f.getName().length();
         }
